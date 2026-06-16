ce707c2f55c27ee6f596cc0d17c86349eaf2fb96
Josh <joshuapankratz07@gmail.com>
2026-06-16 19:01:03 -0400

    Added Client Stopping Event to shut down JDA when Client is stopped.

--- src/main/java/greenebolt/chatdc/registration/ChatListeners.java
+++ src/main/java/greenebolt/chatdc/registration/ChatListeners.java
@@ -3,8 +3,13 @@ package greenebolt.chatdc.registration;
 import greenebolt.chatdc.Config;
 import greenebolt.chatdc.DiscordChatLink;
 import greenebolt.chatdc.utils.Util;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+
+import java.awt.desktop.QuitEvent;
 
 public class ChatListeners {
     public static void register() {
@@ -36,6 +41,10 @@ public class ChatListeners {
                 return;
             }
             Util.SendConfigMessage(client, "No discord channel is defined: Do /setchannel command from discord.");
+        });
+        ClientLifecycleEvents.CLIENT_STOPPING.register((mc) -> {
+            if (!DiscordChatLink.JDAActive) return;
+            Util.Stop("ClientStopped");
         });
     }
 }
